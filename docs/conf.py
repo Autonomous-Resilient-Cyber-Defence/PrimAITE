aa8284897ae9f789604486e7d574a9f798fee7a4
Chris McCarthy <chris.mccarthy@methods.co.uk>
2023-05-25 10:52:29 +0100

    Package restructuring and renaming for 1.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -5,24 +5,37 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+import datetime
+import furo
+sys.path.insert(0, os.path.abspath("../"))
 
-project = 'PrimAITE'
-copyright = '2022, jashort'
-author = 'jashort'
-release = '0.1.0'
+
+# -- Project information -----------------------------------------------------
+year = datetime.datetime.now().year
+project = "primaite"
+copyright = f"Copyright (C) QinetiQ Training and Simulation Ltd 2021 - {year}"
+author = "QinetiQ Training and Simulation Ltd"
+
+# The short Major.Minor.Build version
+with open("../src/primaite/VERSION", "r") as file:
+    version = file.readline()
+# The full version, including alpha/beta/rc tags
+release = version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme']
+extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = []
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
