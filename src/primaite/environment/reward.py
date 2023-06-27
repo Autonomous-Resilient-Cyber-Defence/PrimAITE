cdeb6abf607c81d4f94fa44d6560be0373ddf098
Marek Wolan <marek.wolan@methods.co.uk>
2023-06-27 12:44:42 +0100

    More descriptive debug msg

--- src/primaite/environment/reward.py
+++ src/primaite/environment/reward.py
@@ -78,19 +78,23 @@ def calculate_reward_function(
         start_step = ier_value.get_start_step()
         stop_step = ier_value.get_end_step()
         if step_count >= start_step and step_count <= stop_step:
-            if not ier_value.get_is_running() and reference_ier.get_is_running():
-                # what should happen if reference IER is blocked but live IER is running?
-                _LOGGER.debug(
-                    (
-                        f"Applying penalty of "
-                        f"{config_values.green_ier_blocked * ier_value.get_mission_criticality()} "
-                        f"due to IER {ier_key} being blocked"
+            if not ier_value.get_is_running():
+                if reference_ier.get_is_running():
+                    ier_reward = (
+                        config_values.green_ier_blocked
+                        * ier_value.get_mission_criticality()
                     )
-                )
-                reward_value += (
-                    config_values.green_ier_blocked
-                    * ier_value.get_mission_criticality()
-                )
+                    _LOGGER.debug(
+                        f"Applying reward of {ier_reward} because IER {ier_key} is blocked"
+                    )
+                    reward_value += ier_reward
+                else:
+                    _LOGGER.debug(
+                        (
+                            f"IER {ier_key} is blocked in the reference and live environments. "
+                            f"Therefore, no penalty was applied."
+                        )
+                    )
 
     return reward_value
 
