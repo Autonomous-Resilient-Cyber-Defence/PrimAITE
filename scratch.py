9666b92caa45b96cc007ca5ecf9456e079e12bdc
Marek Wolan <marek.wolan@methods.co.uk>
2023-06-28 11:07:45 +0100

    Attempt to add flat spaces

---
+++ scratch.py
@@ -0,0 +1,6 @@
+from primaite.main import run
+
+run(
+    "/home/cade/repos/PrimAITE/src/primaite/config/_package_data/training/training_config_main.yaml",
+    "/home/cade/repos/PrimAITE/src/primaite/config/_package_data/lay_down/lay_down_config_5_data_manipulation.yaml",
+)
