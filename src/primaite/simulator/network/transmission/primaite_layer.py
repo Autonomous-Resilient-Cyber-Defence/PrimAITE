9d17a9b0d3f4548423510f7c65a4e1f6a5aaaace
Chris McCarthy <chris.mccarthy@methods.co.uk>
2023-08-01 22:25:00 +0100

    #1724 - Added the primaite/simulator/network/transmission sub-package with modules for each layer. They come together to build a minimal but fairly realistic network Frame. A custom PrimaiteHeader has been included to hold primaite specific metadata required in transmission for reward function and RL agent downstream. Added some basic tests that check the proper configuration of Frames with matching headers for protocols. Updated the frame typehints in NIC and Link classes.

---
+++ src/primaite/simulator/network/transmission/primaite_layer.py
@@ -0,0 +1,40 @@
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class DataStatus(Enum):
+    """
+    The status of the data in transmission.
+
+    Members:
+     - GOOD: 1
+     - COMPROMISED: 2
+     - CORRUPT: 3
+    """
+
+    GOOD = 1
+    COMPROMISED = 2
+    CORRUPT = 3
+
+
+class AgentSource(Enum):
+    """
+    The agent source of the transmission.
+
+    Members:
+     - RED: 1
+     - GREEN: 2
+     - BLUE: 3
+    """
+
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+
+
+class PrimaiteHeader(BaseModel):
+    """A custom header for carrying PrimAITE transmission metadata required for RL."""
+
+    agent_source: AgentSource = AgentSource.GREEN
+    data_status: DataStatus = DataStatus.GOOD
