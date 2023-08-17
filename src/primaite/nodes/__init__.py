284bd7709231ef902826048e8cc2174f9b24df23
Chris McCarthy <chris.mccarthy@methods.co.uk>
2023-08-17 16:04:51 +0100

    v2.0.0 sphinx docs build for sphinx-docs-github-pages branch

--- src/primaite/nodes/__init__.py
+++
@@ -1,2 +0,0 @@
-# © Crown-owned copyright 2023, Defence Science and Technology Laboratory UK
-"""Nodes represent network hosts in the simulation."""
--- src/primaite/pol/__init__.py
+++
@@ -1,2 +0,0 @@
-# © Crown-owned copyright 2023, Defence Science and Technology Laboratory UK
-"""Pattern of Life- Represents the actions of users on the network."""
