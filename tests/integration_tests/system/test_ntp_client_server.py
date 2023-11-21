60d94bf4b56f45c2e60a57ce9b2dce663d37feda
Nick Todd <nick.todd@t-s.qinetiq.com>
2023-11-21 11:17:38 +0000

    #2041: Remove test

--- tests/integration_tests/system/test_ntp_client_server.py
+++ tests/integration_tests/system/test_ntp_client_server.py
@@ -40,10 +40,6 @@ def create_ntp_network() -> Network:
     return network
 
 
-# @pytest.fixture()
-# def create_network():
-#     return create_ntp_network()
-
 # Define one node to be an NTP server and another node to be a NTP Client.
 
 
@@ -67,27 +63,28 @@ def test_ntp_client_server():
     assert ntp_client.apply_timestep(1) is True
 
 
+# TODO: Disabled until a service such as NTP can introspect to see if it's running.
 # Test ntp client behaviour when ntp server is unavailable.
-def test_ntp_server_failure():
-    network = create_ntp_network()
-    server: Server = network.get_node_by_hostname("ntp_server")
-    client: Computer = network.get_node_by_hostname("ntp_client")
+# def test_ntp_server_failure():
+#     network = create_ntp_network()
+#     server: Server = network.get_node_by_hostname("ntp_server")
+#     client: Computer = network.get_node_by_hostname("ntp_client")
 
-    ntp_server: NTPServer = server.software_manager.software["NTPServer"]
-    ntp_client: NTPClient = client.software_manager.software["NTPClient"]
+#     ntp_server: NTPServer = server.software_manager.software["NTPServer"]
+#     ntp_client: NTPClient = client.software_manager.software["NTPClient"]
 
-    assert ntp_client.operating_state == ServiceOperatingState.RUNNING
+#     assert ntp_client.operating_state == ServiceOperatingState.RUNNING
 
-    # Turn off ntp server.
-    ntp_server.stop()
-    assert ntp_server.operating_state == ServiceOperatingState.STOPPED
-    # And request a time update.
-    ntp_request = NTPRequest(ntp_client="192.168.1.3")
-    ntp_packet = NTPPacket(ntp_request=ntp_request)
-    ntp_client.send(payload=ntp_packet)
-    assert ntp_server.receive(payload=ntp_packet) is False
-    assert ntp_client.receive(payload=ntp_packet) is False
+#     # Turn off ntp server.
+#     ntp_server.stop()
+#     assert ntp_server.operating_state == ServiceOperatingState.STOPPED
+#     # And request a time update.
+#     ntp_request = NTPRequest(ntp_client="192.168.1.3")
+#     ntp_packet = NTPPacket(ntp_request=ntp_request)
+#     ntp_client.send(payload=ntp_packet)
+#     assert ntp_server.receive(payload=ntp_packet) is False
+#     assert ntp_client.receive(payload=ntp_packet) is False
 
-    # Restart ntp server.
-    ntp_server.start()
-    assert ntp_server.operating_state == ServiceOperatingState.RUNNING
+#     # Restart ntp server.
+#     ntp_server.start()
+#     assert ntp_server.operating_state == ServiceOperatingState.RUNNING
