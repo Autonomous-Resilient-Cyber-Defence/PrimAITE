716bd626a5e67715aa9f5360208c5457880250f2
Marek Wolan <marek.wolan@methods.co.uk>
2024-01-09 14:29:23 +0000

    Hide software health state until scan.

--- src/primaite/simulator/system/software.py
+++ src/primaite/simulator/system/software.py
@@ -145,8 +145,8 @@ class Software(SimComponent):
         state = super().describe_state()
         state.update(
             {
-                "health_state": self.health_state_actual.value,
-                "health_state_red_view": self.health_state_visible.value,
+                "health_state_actual": self.health_state_actual.value,
+                "health_state_visible": self.health_state_visible.value,
                 "criticality": self.criticality.value,
                 "patching_count": self.patching_count,
                 "scanning_count": self.scanning_count,
