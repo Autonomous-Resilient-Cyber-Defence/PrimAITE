d1bbab71b7bce2694cfe07f1ac0109aa9f27bdf4
Marek Wolan <marek.wolan@methods.co.uk>
2024-06-10 14:46:06 +0000

    Updated session_output_writer.py

--- src/primaite/utils/session_output_writer.py
+++ src/primaite/utils/session_output_writer.py
@@ -2,7 +2,7 @@
 raise DeprecationWarning(
     "Benchmarking depends on deprecated functionality and it has not been updated to primaite v3 yet."
 )
-# © Crown-owned copyright 2023, Defence Science and Technology Laboratory UK
+# © Crown-owned copyright 2024, Defence Science and Technology Laboratory UK
 import csv
 from logging import Logger
 from typing import Final, List, Tuple, TYPE_CHECKING, Union
