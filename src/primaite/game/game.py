56e81a020ca31c8a8770d7a737f8008166474b50
Charlie Crane <Charlie.Crane@t-s.qinetiq.com>
2025-02-11 10:53:41 +0000

    #3060 - Adding version to YAML files within PrimAITE

--- src/primaite/game/game.py
+++ src/primaite/game/game.py
@@ -247,6 +247,11 @@ class PrimaiteGame:
         game.options = PrimaiteGameOptions(**cfg["game"])
         game.save_step_metadata = cfg.get("io_settings", {}).get("save_step_metadata") or False
 
+        # TODO: Future YAML config should specify the PrimAITE version they are written for.
+        #       For now, we warn that if it is missing, pending a mechanism to handle variations.
+        if not cfg["version"]:
+            _LOGGER.warning("Version definition is missing from provided configuration. ")
+
         # 1. create simulation
         sim = game.simulation
         net = sim.network
